375a2ffb821092da9c4df71d47cb669d5a2c6d23
Supan Roy <140494154+Supan-Roy@users.noreply.github.com>
2025-09-15 20:52:44 +0600

    Initial commit

---
+++ OFFICE_Chef in his Office.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x, y;
+	    cin>>x>>y;
+	    int ans = x*4+y;
+	    cout<<ans<<endl;
+	}
+	return 0;
+}
